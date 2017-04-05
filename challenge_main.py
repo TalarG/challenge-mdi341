8653e925bc57eae9c06e34b0fe9cec427b87d4de
matthibou <mattguillouet@gmail.com>
2017-04-05 11:39:42 +0200

    add evaluation on the training set + display best and worst images

--- challenge_main.py
+++ challenge_main.py
@@ -79,7 +79,7 @@ _, predictions_size = train_template_data.shape
 
 max_epoch = 300
 batch_train = 400
-batch_test = 200
+batch_test = 400
 
 epoch_step = batch_train / nb_img_train
 nbiter_epoch = np.floor(nb_img_train / batch_train)
@@ -93,6 +93,9 @@ full_dir = join(summary_dir, folder_name)
 
 validation_log_frequency = 5
 validation_log_frequency_iter = np.floor(validation_log_frequency / epoch_step)
+
+evaluation_log_frequency = 20
+evaluation_log_frequency_iter = np.floor(evaluation_log_frequency / epoch_step)
 
 training_log_frequency = 0.5
 training_log_frequency_iter = np.floor(training_log_frequency / epoch_step)
@@ -124,31 +127,31 @@ filter_nb_1 = 10
 filter_nb_2 = 15
 filter_nb_3 = 20
 
-hidden1 = conv_layer(x_, [filter_size, filter_size, 1, filter_nb_1], 'conv_1', stride, keep_prob, is_training)
-hidden2 = conv_layer(hidden1, [filter_size, filter_size, filter_nb_1, filter_nb_1], 'conv_2', stride, keep_prob, is_training)
+hidden1 = conv_layer(x_, [filter_size, filter_size, 1, filter_nb_1], 'conv-1', stride, keep_prob, is_training)
+hidden2 = conv_layer(hidden1, [filter_size, filter_size, filter_nb_1, filter_nb_1], 'conv-2', stride, keep_prob, is_training)
 
 pool3 = tf.nn.max_pool(hidden2, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME', data_format='NHWC', name=None)
 
-hidden4 = conv_layer(pool3, [filter_size, filter_size, filter_nb_1, filter_nb_2], 'conv_4', stride, keep_prob, is_training)
-hidden5 = conv_layer(hidden4, [filter_size, filter_size, filter_nb_2, filter_nb_2], 'conv_5', stride, keep_prob, is_training)
+hidden4 = conv_layer(pool3, [filter_size, filter_size, filter_nb_1, filter_nb_2], 'conv-4', stride, keep_prob, is_training)
+hidden5 = conv_layer(hidden4, [filter_size, filter_size, filter_nb_2, filter_nb_2], 'conv-5', stride, keep_prob, is_training)
 
 pool6 = tf.nn.max_pool(hidden5, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME', data_format='NHWC', name=None)
 
-hidden7 = conv_layer(pool6, [filter_size, filter_size, filter_nb_2, filter_nb_3], 'conv_7', stride, keep_prob, is_training)
-hidden8 = conv_layer(hidden7, [filter_size, filter_size, filter_nb_3, filter_nb_3], 'conv_8', stride, keep_prob, is_training)
+hidden7 = conv_layer(pool6, [filter_size, filter_size, filter_nb_2, filter_nb_3], 'conv-7', stride, keep_prob, is_training)
+hidden8 = conv_layer(hidden7, [filter_size, filter_size, filter_nb_3, filter_nb_3], 'conv-8', stride, keep_prob, is_training)
 
 pool9 = tf.nn.max_pool(hidden8, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME', data_format='NHWC', name=None)
 
-hidden10 = conv_layer(pool9, [filter_size, filter_size, filter_nb_3, filter_nb_3], 'conv_10', stride, keep_prob, is_training)
-hidden11 = conv_layer(hidden10, [filter_size, filter_size, filter_nb_3, filter_nb_3], 'conv_11', stride, keep_prob, is_training)
+hidden10 = conv_layer(pool9, [filter_size, filter_size, filter_nb_3, filter_nb_3], 'conv-10', stride, keep_prob, is_training)
+hidden11 = conv_layer(hidden10, [filter_size, filter_size, filter_nb_3, filter_nb_3], 'conv-11', stride, keep_prob, is_training)
 
 pool12 = tf.nn.max_pool(hidden11, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME', data_format='NHWC', name=None)
 
 hidden13 = tf.reshape(pool12, shape=[-1, 3 * 3 * filter_nb_3])
 
-y = fc_layer(hidden13, [3 * 3 * filter_nb_3, template_dim], 'fc_final', keep_prob, act=None)
+y = fc_layer(hidden13, [3 * 3 * filter_nb_3, template_dim], 'fc-final', keep_prob, act=None)
 
-
+squared_error = tf.reduce_sum(tf.square(y - y_), axis=1)
 
 #############################################
 ################ THE LOSS ###################
@@ -156,7 +159,7 @@ y = fc_layer(hidden13, [3 * 3 * filter_nb_3, template_dim], 'fc_final', keep_pro
 
 """ Loss for regression """
 with tf.name_scope('training'):
-	euclidean_loss = tf.reduce_mean(tf.square(y - y_))
+	euclidean_loss = tf.reduce_mean(y - y_)
 
 tf.summary.scalar('train_euclidean_loss', euclidean_loss)
 
@@ -185,8 +188,31 @@ with tf.name_scope('validation'):
 summary_validation_loss = tf.summary.scalar('validation_euclidean_loss', validation_loss)
 
 
-##########################################################################################################
+############ IMAGE SUMMARIES
+with tf.name_scope('display_training_images'):
 
+  	training_high_variance_images = tf.placeholder(tf.float32,
+    								[None, image_dim, image_dim, 1], name='high_variance')
+    training_low_variance_images = tf.placeholder(tf.float32,
+    								[None, image_dim, image_dim, 1], name='low_variance')
+    training_high_error_images = tf.placeholder(tf.float32,
+    								[None, image_dim, image_dim, 1], name='high_error')
+    training_low_error_images = tf.placeholder(tf.float32,
+    								[None, image_dim, image_dim, 1], name='low_error')
+
+    summary_high_variance_images = tf.summary.image('high_variance', training_high_variance_images, 5)
+    summary_low_variance_images = tf.summary.image('low_variance', training_low_variance_images, 5)
+    summary_high_error_images = tf.summary.image('high_error', training_high_error_images, 5)
+    summary_low_error_images = tf.summary.image('low_error', training_low_error_images, 5)
+
+
+summary_images = tf.summary.merge([summary_high_variance_images, 
+								summary_low_variance_images,
+								summary_high_error_images,
+								summary_low_error_images])
+
+
+##########################################################################################################
 if tf.gfile.Exists(full_dir):
 	var = input('The folder {:s} already exists.' + ' Would you like to overwrite it ?\n' + 'yes(y), no(n): '.format(full_dir))
 
@@ -198,12 +224,13 @@ if tf.gfile.Exists(full_dir):
 else:
 	tf.gfile.MakeDirs(full_dir)
 
-sess = tf.InteractiveSession()
+sess = tf.Session()
 
 train_writer = tf.summary.FileWriter(full_dir + '/train', sess.graph)
 validation_writer = tf.summary.FileWriter(full_dir + '/validation')
 
-tf.global_variables_initializer().run()
+init = tf.global_variables_initializer()
+sess.run(init)
 
 ##########################################################################################################
 ##########################################################################################################
@@ -215,8 +242,6 @@ test_imgs = test_imgs.reshape([-1, image_dim, image_dim, 1])
 
 X_train, y_train = shuffle(train_imgs, train_template_data, random_state=42)
 
-#X_train, y_train = (train_imgs, train_template_data)
-
 cursors = Cursors()
 
 
@@ -225,7 +250,7 @@ def feed_func(batch_size, mode='train', placeholder_dict=placeholder_dict, curso
 	if mode == 'train':
 
 		tmp_cur = cursors.train_current_pos
-		ind_batch = np.mod(np.arange(tmp_cur, tmp_cur + batch_size), nb_img_train).astype(int)
+		ind_batch = np.mod(tmp_cur + np.arange(batch_size), nb_img_train).astype(int)
 		
 		X_tmp = X_train[ind_batch]
 		y_tmp = y_train[ind_batch]
@@ -237,7 +262,7 @@ def feed_func(batch_size, mode='train', placeholder_dict=placeholder_dict, curso
 	elif mode == 'valid':
 
 		tmp_cur = cursors.validation_current_pos
-		ind_batch = np.mod(np.arange(tmp_cur, tmp_cur + batch_size), nb_img_valid)
+		ind_batch = np.mod(tmp_cur + np.arange(batch_size), nb_img_valid).astype(int)
 		
 		X_tmp = valid_imgs[ind_batch]
 		y_tmp = valid_template_data[ind_batch]
@@ -246,15 +271,31 @@ def feed_func(batch_size, mode='train', placeholder_dict=placeholder_dict, curso
 
 		is_training_tmp = 0.0
 
+	# non shuffled dataset
+	elif mode == 'eval':
+
+		tmp_cur = cursors.eval_current_pos
+		ind_batch = np.mod(tmp_cur + np.arange(batch_size), nb_img_train).astype(int)
+		
+		X_tmp = train_imgs[ind_batch]
+		y_tmp = train_template_data[ind_batch]
+
+		cursors.eval_current_pos = ind_batch[-1] + 1
+
+		is_training_tmp = 0.0
+
 	return {placeholder_dict['x_']: X_tmp, placeholder_dict['y_']: y_tmp, placeholder_dict['keep_prob']: dropout, placeholder_dict['is-training']: is_training_tmp}
 
 
+
+##################### TRAINING LOOP #####################"""
 i = 0
-nb_iter_validation = np.ceil(nb_img_valid /batch_test)
+nb_iter_validation = np.ceil(nb_img_valid / batch_test)
+nb_iter_evaluation = np.ceil(nb_img_train / batch_test)
 
 while i < nb_max_iter:
 
-	####################### TEST MODE ############################
+	####################### VALIDATION MODE ############################
 	if ((np.mod(i, validation_log_frequency_iter) == 0) & (not i == 0)):
 
 		cursors.validation_current_pos = 0
@@ -262,8 +303,7 @@ while i < nb_max_iter:
 		montecarlo_samples_validation = np.zeros((nb_img_valid, template_dim, nb_montecarlo_predictions), dtype=np.float32)
 		
 		for jj in np.arange(nb_iter_validation):
-			
-			ind_tmp = np.mod(np.arange(jj * batch_test, (jj + 1) * batch_test), nb_img_valid).astype(int)
+			ind_tmp = np.mod(jj * batch_test + np.arange(batch_test), nb_img_valid).astype(int)
 			feed_dict = feed_func(batch_test, mode='valid')
 
 			for kk in np.arange(nb_montecarlo_predictions):
@@ -283,7 +323,8 @@ while i < nb_max_iter:
 	####################### TRAIN MODE ############################
 	if ((np.mod(i, training_log_frequency_iter) == 0) & (not i == 0)):
 
-		train_sum, _, loss = sess.run([merged_train_summary, train_op, euclidean_loss], feed_dict=feed_func(batch_train, mode='train'))
+		train_sum, _, loss, squared_error = sess.run([merged_train_summary, train_op, euclidean_loss, squared_error], feed_dict=feed_func(batch_train, mode='train'))
+		
 		train_writer.add_summary(train_sum, i)
 
 		####
@@ -291,6 +332,42 @@ while i < nb_max_iter:
 
 	else:
 		_ = sess.run(train_op, feed_dict=feed_func(batch_train, mode='train'))
+
+
+	##################### EVAL ON TRAIN DATASET ###################
+	if ((np.mod(i, evaluation_log_frequency_iter) == 0) & (not i == 0)):
+
+
+		cursors.eval_current_pos = 0
+		
+		montecarlo_samples_evaluation = np.zeros((nb_img_train, template_dim, nb_montecarlo_predictions), dtype=np.float32)
+		
+		for jj in np.arange(nb_iter_evaluation):
+			ind_tmp = np.mod(jj * batch_test + np.arange(batch_test), nb_img_train).astype(int)
+			feed_dict = feed_func(batch_test, mode='eval')
+
+			for kk in np.arange(nb_montecarlo_predictions):
+				mc_sample = sess.run(y, feed_dict=feed_dict)
+				montecarlo_samples_validation[ind_tmp, :, kk] = mc_sample
+
+		montecarlo_predictions_evaluation = np.mean(montecarlo_samples_validation, axis=2)
+		#centred_prediction_evaluation = montecarlo_samples_evaluation - montecarlo_predictions_evaluation.reshape(-1, -1, 1)
+
+
+		train_squared_error = np.sum((montecarlo_predictions_evaluation - train_template_data)** 2, axis=1)
+
+
+		sorted_ind = np.argsort(train_squared_error)
+		high_error_ind = sorted_ind[-5:]
+		low_error_ind = sorted_ind[:5]
+
+		feed_images = {training_high_error_images: train_imgs[high_error_ind], 
+					training_low_error_images:train_imgs[low_error_ind]}
+
+		sum_high_err_img, sum_low_err_img=sess.run([summary_high_error_images, summary_low_error_images], feed_dict=feed_images)
+
+		train_writer.add_summary(sum_high_err_img, epoch)
+		train_writer.add_summary(sum_low_err_img, epoch)
 
 	i += 1
 
