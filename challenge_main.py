17b6c98d3011cb2c3a1435fb7dc6f462e4d5dd78
matthibou <mattguillouet@gmail.com>
2017-04-20 19:20:37 +0200

    name

--- challenge_main.py
+++ challenge_main.py
@@ -118,7 +118,7 @@ if pre_processing:
 if batch_norm:
 	folder_name += '_batchnorm'
 
-folder_name += '_ultra_deep_4layers'
+folder_name += '_ultra_deep_4layers_small_filters'
 
 full_dir = join(summary_dir, folder_name)
 
