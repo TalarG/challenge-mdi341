5f3fbd9e510294cfd9fafaf783d17b17849f878b
matthibou <mattguillouet@gmail.com>
2017-04-21 19:03:37 +0200

    new architecture 3322

--- challenge_main.py
+++ challenge_main.py
@@ -200,17 +200,17 @@ pool10 = tf.nn.max_pool(hidden8, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME', dat
 
 hidden11 = conv_layer(pool10, [filter_size, filter_size, filter_nb_2, filter_nb_3], 'conv-9', stride, keep_prob, is_training, act=activation_func)
 hidden12 = conv_layer(hidden11, [filter_size, filter_size, filter_nb_3, filter_nb_3], 'conv-10', stride, keep_prob, is_training, act=activation_func)
-hidden13 = conv_layer(hidden12, [filter_size, filter_size, filter_nb_3, filter_nb_3], 'conv-11', stride, keep_prob, is_training, act=activation_func)
+#hidden13 = conv_layer(hidden12, [filter_size, filter_size, filter_nb_3, filter_nb_3], 'conv-11', stride, keep_prob, is_training, act=activation_func)
 #hidden14 = conv_layer(hidden13, [filter_size, filter_size, filter_nb_3, filter_nb_3], 'conv-12', stride, keep_prob, is_training, act=activation_func)
 
-pool15 = tf.nn.max_pool(hidden13, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME', data_format='NHWC', name=None)
+pool15 = tf.nn.max_pool(hidden12, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME', data_format='NHWC', name=None)
 
 hidden16 = conv_layer(pool15, [filter_size, filter_size, filter_nb_3, filter_nb_4], 'conv-13', stride, keep_prob, is_training, act=activation_func)
 hidden17 = conv_layer(hidden16, [filter_size, filter_size, filter_nb_4, filter_nb_4], 'conv-14', stride, keep_prob, is_training, act=activation_func)
-hidden18 = conv_layer(hidden17, [filter_size, filter_size, filter_nb_4, filter_nb_4], 'conv-15', stride, keep_prob, is_training, act=activation_func)
+#hidden18 = conv_layer(hidden17, [filter_size, filter_size, filter_nb_4, filter_nb_4], 'conv-15', stride, keep_prob, is_training, act=activation_func)
 #hidden19 = conv_layer(hidden18, [filter_size, filter_size, filter_nb_4, filter_nb_4], 'conv-16', stride, keep_prob, is_training, act=activation_func)
 
-pool20 = tf.nn.max_pool(hidden18, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME', data_format='NHWC', name=None)
+pool20 = tf.nn.max_pool(hidden17, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME', data_format='NHWC', name=None)
 
 '''
 hidden21 = conv_layer(pool20, [filter_size, filter_size, filter_nb_4, filter_nb_5], 'conv-17', stride, keep_prob, is_training, act=activation_func)
