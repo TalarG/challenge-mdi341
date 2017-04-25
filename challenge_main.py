8a0712431b132bf4363e0ef84d80be1f47e2f64e
matthibou <mattguillouet@gmail.com>
2017-04-25 20:16:29 +0200

    back to old config

--- challenge_main.py
+++ challenge_main.py
@@ -92,7 +92,7 @@ nb_img_test, _ = test_imgs.shape
 _, predictions_size = train_template_data.shape
 
 max_epoch = 1500
-batch_train = 75 #800 best
+batch_train = 50
 batch_test = 500
 
 epoch_step = batch_train / nb_img_train
@@ -108,8 +108,8 @@ batch_norm = False
 nb_montecarlo_predictions = 20
 
 pre_processing = True
-power_pca = - 1 / 4
-nb_kept_components = 1800
+power_pca = - 1 / 5
+nb_kept_components = 2000
 
 summary_dir = '../tensorlog'
 folder_name = 'epoch_%i_dp_%.2f_nbmcdp_%i' % (max_epoch, dropout, nb_montecarlo_predictions)
@@ -142,9 +142,8 @@ nb_display_images = 8
 #####################################################################################################################
 if pre_processing:
 	pca = PCA(svd_solver='randomized', n_components=nb_kept_components)
-	pca.fit(train_imgs[::2])
+	pca.fit(train_imgs)
 
-	#power_pca = np.flipud(np.linspace(0, -1 / 3, nb_kept_components))
 	pca_preprocess = lambda x: x.dot(pca.components_.T).dot(pca.components_ * np.power(pca.explained_variance_, power_pca).reshape(-1,1))
 
 	train_imgs = pca_preprocess(train_imgs)
